3356a56e24f8be3edc289e00ebea79ebecdb625b
Gazmir Mazari <37550822+GazmirDi@users.noreply.github.com>
2022-11-12 11:16:42 -0500

    set up the authentication sign up files

---
+++ pages/api/signup.ts
@@ -0,0 +1,10 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import cookie from 'cooki'
+import { PrismaClient } from '../../lib/prismaClient'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+
+}
